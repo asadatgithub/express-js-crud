41a9530650ef0f8158bf1a400a7a9322d2b57c5c
Asad Kareem <asadghauri09@gmail.com>
2022-11-16 10:15:28 +0500

    Added JWT Authentication

--- routes/books.js
+++ routes/books.js
@@ -1,20 +1,23 @@
 
 import express from "express";
-import mongoose from "mongoose"
 import Book from "../schemas/book.js"
+
+import authenticate from "../middlewares/authentication.js";
 const router = express.Router();
 
-router.get("/", async (req, res) => {
- 
+router.get("/", authenticate, async (req, res) => {
+
     try {
         const books = await Book.find();
         res.json(books);
     } catch (err) {
         res.json({ message: err });
-    }
+    } o
 })
 
-router.get("/:id", async (req, res) => {
+
+
+router.get("/:id", authenticate, async (req, res) => {
     try {
         const book = await Book.findById(req.params.id);
         res.json(book);
@@ -23,7 +26,7 @@ router.get("/:id", async (req, res) => {
     }
 })
 
-router.post("/", async (req, res) => {
+router.post("/", authenticate, async (req, res) => {
     try {
         const result = await Book.create(req.body);
         res.status(200).json(result);
@@ -32,7 +35,7 @@ router.post("/", async (req, res) => {
     }
 })
 
-router.patch("/:id", async (req, res) => {
+router.patch("/:id", authenticate, async (req, res) => {
     try {
         const result = await Book.findByIdAndUpdate(req.params.id, req.body);
         res.status(200).json(result);
@@ -41,7 +44,7 @@ router.patch("/:id", async (req, res) => {
     }
 })
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", authenticate, async (req, res) => {
     try {
         const result = await Book.findByIdAndDelete(req.params.id);
         res.status(200).json(result);
---
+++ routes/user.js
@@ -0,0 +1,30 @@
+
+import express from "express";
+import User from "../schemas/user.js"
+import jwt from "jsonwebtoken";
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email, password: req.body.password });
+        if (!user) return res.status(401).send("Invalid email or password.");
+        const token = jwt.sign({ id: user._id }, "my_temporary_secret", { expiresIn: "1h" });
+        res.send(token);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+
+router.post("/sign-up", async (req, res) => {
+    try{
+        const result = await User.create(req.body);
+        res.status(200).json(result);
+    }catch(error){
+        res.status(500).send(error);
+    }
+
+})
+
+
+export default router;
